e849ef56d3a36151786b1f52c0fdffe88879378b
tnovak123 <timothy.m.novak@gmail.com>
2018-04-14 11:13:28 -0500

    Added ch 4 code, flask_migrate not importing otherwise database interface code added.

--- config.py
+++ config.py
@@ -1,4 +1,11 @@
 import os
 
+
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://microblog:blog@localhost:8889/microblog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    
