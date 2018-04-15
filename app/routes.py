020579ebc8e9c403c14ed8247fac43a562e13a5c
tnovak123 <timothy.m.novak@gmail.com>
2018-04-14 23:17:49 -0500

    adjusted routes page.

--- app/routes.py
+++ app/routes.py
@@ -9,7 +9,17 @@ from app.models import User
 @app.route('/index')
 @login_required
 def index():
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body' : 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body' : 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
