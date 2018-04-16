6795543e8fc5ccae65150bc7c1fe84ef9c6be6b7
tnovak123 <timothy.m.novak@gmail.com>
2018-04-15 22:30:40 -0500

    Ch 8 complete, but untested. Added follow and unfollow capability, updated database to accomodate new featurs, added testing algorithms.

--- app/forms.py
+++ app/forms.py
@@ -30,3 +30,14 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kargs):
+        super(EditProfileForm, self).__init__(*args, **kargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
--- config.py
+++ config.py
@@ -3,9 +3,18 @@ import os
 
 
 class Config(object):
+    # Secret Key that prevents cross site code injection
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # Debug feature
     DEBUG = True
+    # Database interface settings
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://microblog:blog@localhost:8889/microblog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    
+    # Mail notification settings
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL-PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com', 'error-messages@server.com']
