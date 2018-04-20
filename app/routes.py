1a9514ecbb6f67eca5dc4239fe4dad7004bda8ce
tnovak123 <timothy.m.novak@gmail.com>
2018-04-19 19:14:55 -0500

    Ch 11 complete and working.

--- app/routes.py
+++ app/routes.py
@@ -98,7 +98,7 @@ def user(username):
     prev_url = url_for('user', username=user.username, page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
-
+    
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
